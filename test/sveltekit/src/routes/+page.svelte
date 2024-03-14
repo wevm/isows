6a9d8384e147ba3c14bce5091f56656cad9b4fa5
moxey.eth <jakemoxey@gmail.com>
2024-03-15 07:36:37 +1100

    chore: lint

--- test/sveltekit/src/routes/+page.svelte
+++ test/sveltekit/src/routes/+page.svelte
@@ -1,16 +1,15 @@
 <script>
-  import { WebSocket } from 'isows'
+import { WebSocket } from "isows";
 
-  let promise = ws()
-  async function ws() {
-    await new Promise((res) => {
-      const ws = new WebSocket(
-        "wss://ws-ap1.pusher.com:443/app/78a7f9604f977d235435",
-      );
-      ws.onopen = () => res(null);
-    });
-
-  }
+const promise = ws();
+async function ws() {
+  await new Promise((res) => {
+    const ws = new WebSocket(
+      "wss://ws-ap1.pusher.com:443/app/78a7f9604f977d235435",
+    );
+    ws.onopen = () => res(null);
+  });
+}
 </script>
 
 <div>server: success</div>
